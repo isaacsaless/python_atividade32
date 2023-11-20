b28f6e76ff66a2e415ec34a2453e187263276438
Isaac Nascimento Sales <121314776+isaacsaless@users.noreply.github.com>
2023-11-20 15:58:49 -0300

    primeira versão do código para calcular o salário mensal de alguém

---
+++ salariomes.py
@@ -0,0 +1,6 @@
+def calcMes(a,b):
+    salariomes = a*b
+    print(f'Você ganha R${salariomes} por mês.')
+dinheiroHora = float(input("Quantos reais você ganha por hora no trabalho: "))
+horaMes = int(input("Quantas horas você trabalha por mês: "))
+calcMes(dinheiroHora,horaMes)
